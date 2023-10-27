1045b2f5fc2b2dbb4b51f968733021ad92c2ae47
Seongwon <sngoneee@gmail.com>
2023-10-27 15:16:20 +0900

    slot

--- vuestudy/src/main.js
+++ vuestudy/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js'
 
+import mitt from 'mitt'
+let emitter = mitt();
+let app = createApp(App);
+app.config.globalProperties.emitter = emitter;
 
-createApp(App).use(router).mount('#app') 
-
+app.mount('#app') 
