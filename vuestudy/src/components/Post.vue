ad8566789b6d24f4420a2c03189ee37189626c95
Seongwon <sngoneee@gmail.com>
2023-10-27 15:16:35 +0900

    slot

--- vuestudy/src/components/Post.vue
+++ vuestudy/src/components/Post.vue
@@ -5,7 +5,7 @@
           <div class="profile" :style="{ backgroundImage : `url(${post.userImage})` }"></div>
           <span class="profile-name">{{post.name}}</span>
         </div>
-        <div class="post-body" :style="{ backgroundImage : `url(${post.postImage})` }"></div>
+        <div class="post-body" :style="{ backgroundImage : `url(${post.postImage})` }" :class="`${post.filter}`"></div>
         <div class="post-content">
           <p>{{post.likes}} Likes</p>
           <p><strong>{{post.name}}</strong> {{post.content}}</p>
@@ -18,7 +18,7 @@
 
 export default {
   props : {
-    post : Array 
+    post : Array
   }
   
 }
