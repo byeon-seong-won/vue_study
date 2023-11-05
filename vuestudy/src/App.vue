307eec50f64e754209dff6305294be4fef4e98f7
seong <sngoneee@gmail.com>
2023-11-05 20:27:40 +0900

    study

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="menu">
+      <a>Home</a>
+      <a>Products</a>
+      <a>About</a>
+    </div>
     <div>
       <h4>XX 원룸</h4>
       <p>XX 만원</p>
@@ -32,7 +37,15 @@ export default {
 
 <style>
 * {font-family: 'Noto sans KR', sans-serif; letter-spacing: -0.03rem;font-size: 20px;}
-
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
+}
 
 
 </style>
