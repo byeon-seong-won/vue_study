5ed3c13a9bd7c38203b3d00da6655e77da44c4a7
Seongwon <sngoneee@gmail.com>
2023-11-06 17:50:22 +0900

    study

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
+    <div class="black-bg" v-if="modalOpen == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지내용임</p>
+        <button @click="modalOpen=false">닫기</button>
+      </div>
+    </div>
     <div class="menu">
       <a>Home</a>
       <a>Products</a>
       <a>About</a>
     </div>
     <div>
-      <h4>XX 원룸</h4>
-      <p>XX 만원</p>
+      <h4 @click="modalOpen=true">{{products[0]}}</h4>
+      <p>50만원</p>
+      <button @click="btnClick(0)">신고하기</button>
+      <span>신고수 : {{신고횟수[0]}}</span>
+      <img src="./assets/pic26.jpg" alt="img">
     </div>
     <div>
-      <h4>XX 원룸</h4>
-      <p> {{가격}} 만원</p>
+      <h4 @click="modalOpen=true">{{products[1]}}</h4>
+      <p>50만원</p>
+      <button @click="btnClick(1)">신고하기</button>
+      <span>신고수 : {{신고횟수[1]}}</span>
+      <img src="./assets/pic27.jpg" alt="img">
     </div>
+    <div>
+      <h4 @click="modalOpen=true">{{products[2]}}</h4>
+      <p>50만원</p>
+      <button @click="btnClick(2)">신고하기</button>
+      <span>신고수 : {{신고횟수[2]}}</span>
+      <img src="./assets/pic28.jpg" alt="img">
+    </div>
+    <List :post="post"></List>
   </div>
 </template>
 
@@ -20,14 +41,26 @@
 
 
 <script>
-
-
+import posts from './Post'
+import List from './components/List.vue'
 export default {
   name : 'App',
   data() {
     return {
       가격 : 100,
-      style : 'color:blue'
+      products : ['역삼동', '천호동','마포구'],
+      신고횟수 : [0,0,0],
+      style : 'color:blue',
+      modalOpen : false,
+      post : posts
+    }
+  },
+  components : {
+    List
+  },
+  methods : {
+    btnClick(i) {
+      this.신고횟수[i]++;
     }
   }
 }
@@ -46,6 +79,22 @@ export default {
   color : white;
   padding : 10px;
 }
-
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+img {width: 100%;}
 
 </style>
