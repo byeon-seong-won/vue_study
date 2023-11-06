a61e4f20ac59521b243ec5ecdff7920f30daa0d1
Seongwon <sngoneee@gmail.com>
2023-11-06 17:51:55 +0900

    study

---
+++ vuestudy/src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h5>{{post[0].title}}</h5>
+      <p>블로그 글 날짜</p>
+    </div>
+    <div>
+      <h5>{{post[1].title}}</h5>
+      <p>블로그 글 날짜</p>
+    </div>
+    <div>
+      <h5>{{post[2].title}}</h5>
+      <p>블로그 글 날짜</p>
+    </div>
+  </template>
+  <script>
+  export default {
+    props : {
+        post : Array
+    }
+  }
+  </script>
+  <style>
+  
+  </style>
