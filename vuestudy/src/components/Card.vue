50734919b1034e3623393645376442cad4ce0b47
Seongwon <sngoneee@gmail.com>
2023-11-07 16:14:26 +0900

    study

---
+++ vuestudy/src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="black-bg">
+        <div class="white-bg">
+          <h4>{{ post.title }}</h4>
+          <p>{{ post.content }}</p>
+          <p>{{ post.price }} 원</p>
+          <button @click="$emit('modalClose')">닫기</button>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name : 'Card',
+  props : {
+   
+    
+  }
+}
+
+
+</script>
+
+<style>
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+</style>
