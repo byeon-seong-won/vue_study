e8b463ba981a7af7d0f5166588a00392f5c156b9
Seongwon <sngoneee@gmail.com>
2023-11-07 15:36:11 +0900

    study

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,37 +1,17 @@
 <template>
   <div>
-    <div class="black-bg" v-if="modalOpen == true">
-      <div class="white-bg">
-        <h4>상세페이지</h4>
-        <p>상세페이지내용임</p>
-        <button @click="modalOpen=false">닫기</button>
-      </div>
-    </div>
     <div class="menu">
       <a>Home</a>
       <a>Products</a>
       <a>About</a>
     </div>
-    <div>
-      <h4 @click="modalOpen=true">{{products[0]}}</h4>
+    <div v-for="(a,i) in products" :key="i">
+      <h4 @click="modalOpen=true; modalIdx=i">{{products[i]}}</h4>
+      <Modal v-if="modalOpen == true" :post="post[i]" :modalClose="modalOpen = false"></Modal>
       <p>50만원</p>
-      <button @click="btnClick(0)">신고하기</button>
-      <span>신고수 : {{신고횟수[0]}}</span>
+      <button @click="btnClick(i)">신고하기</button>
+      <span>신고수 : {{신고횟수[i]}}</span>
       <img src="./assets/pic26.jpg" alt="img">
-    </div>
-    <div>
-      <h4 @click="modalOpen=true">{{products[1]}}</h4>
-      <p>50만원</p>
-      <button @click="btnClick(1)">신고하기</button>
-      <span>신고수 : {{신고횟수[1]}}</span>
-      <img src="./assets/pic27.jpg" alt="img">
-    </div>
-    <div>
-      <h4 @click="modalOpen=true">{{products[2]}}</h4>
-      <p>50만원</p>
-      <button @click="btnClick(2)">신고하기</button>
-      <span>신고수 : {{신고횟수[2]}}</span>
-      <img src="./assets/pic28.jpg" alt="img">
     </div>
     <List :post="post"></List>
   </div>
@@ -43,6 +23,7 @@
 <script>
 import posts from './Post'
 import List from './components/List.vue'
+import Modal from './components/Modal.vue'
 export default {
   name : 'App',
   data() {
@@ -52,11 +33,13 @@ export default {
       신고횟수 : [0,0,0],
       style : 'color:blue',
       modalOpen : false,
-      post : posts
+      post : posts,
+      modalIdx : ''
     }
   },
   components : {
-    List
+    List,
+    Modal
   },
   methods : {
     btnClick(i) {
@@ -85,16 +68,7 @@ body {
 div {
   box-sizing: border-box;
 }
-.black-bg {
-  width: 100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
-}
-.white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
-} 
+
 img {width: 100%;}
 
 </style>
