5decc9f24fddd489cf54d323f0ec5ef6c1c9190e
Seongwon <sngoneee@gmail.com>
2023-11-07 17:36:42 +0900

    study

--- vuestudy/src/components/Card.vue
+++ vuestudy/src/components/Card.vue
@@ -1,22 +1,24 @@
+
+
+
+
 <template>
-    <div class="black-bg">
-        <div class="white-bg">
-          <h4>{{ post.title }}</h4>
-          <p>{{ post.content }}</p>
-          <p>{{ post.price }} 원</p>
-          <button @click="$emit('modalClose')">닫기</button>
-        </div>
+    <div>
+        <h4 @click="$emit('modalClick', 해당원룸.id)">{{해당원룸.title}}</h4>
+        <p>{{해당원룸.content}}만원</p>
+        <p>{{해당원룸.price}}만원</p>
+        <img :src="해당원룸.image" alt="img">
+        <!-- <button @click="$emit=('btnClick')">허위매물신고</button>
+        <span>신고수 : {{신고수}}</span> -->
     </div>
 </template>
-
-
 
 <script>
 export default {
   name : 'Card',
   props : {
-   
-    
+    해당원룸 : Object,
+    신고수 : Number
   }
 }
 
