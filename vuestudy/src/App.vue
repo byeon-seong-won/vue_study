a26bdbb4881f69e5dfe213d9127c49a0ce99516d
Seongwon <sngoneee@gmail.com>
2023-11-07 16:14:13 +0900

    study

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
-    <div class="menu">
-      <a>Home</a>
-      <a>Products</a>
-      <a>About</a>
+  <div class="black-bg" v-if="isModalopen == true" :modalIdx="modalIdx">
+    <div class="white-bg">
+      <h4>{{원룸들[modalIdx].title}}</h4>
+      <p>{{원룸들[modalIdx].content}}만원</p>
+      <p>{{원룸들[modalIdx].price}}만원</p>
+      <img :src="원룸들[modalIdx].image" alt="img">
+      <button @click="isModalopen=false">닫기</button>
     </div>
-    <div v-for="(a,i) in products" :key="i">
-      <h4 @click="modalOpen=true; modalIdx=i">{{products[i]}}</h4>
-      <Modal v-if="modalOpen == true" :post="post[i]" :modalClose="modalOpen = false"></Modal>
-      <p>50만원</p>
-      <button @click="btnClick(i)">신고하기</button>
-      <span>신고수 : {{신고횟수[i]}}</span>
-      <img src="./assets/pic26.jpg" alt="img">
-    </div>
-    <List :post="post"></List>
+  </div>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{menu}}</a>
+  </div>
+  <div v-for="(a,i) in 원룸들" :key="i">
+    <h4 @click="isModalopen=true; modalIdx=i">{{원룸들[i].title}}</h4>
+    <p>{{원룸들[i].content}}만원</p>
+    <p>{{원룸들[i].price}}만원</p>
+    <img :src="원룸들[i].image" alt="img">
+    <button @click="신고수[i]++">허위매물신고</button>
+    <span>신고수 : {{신고수[i]}}</span>
   </div>
 </template>
 
@@ -21,54 +25,56 @@
 
 
 <script>
-import posts from './Post'
-import List from './components/List.vue'
-import Modal from './components/Modal.vue'
+import data from './data'
 export default {
   name : 'App',
-  data() {
+  data(){
     return {
-      가격 : 100,
-      products : ['역삼동', '천호동','마포구'],
-      신고횟수 : [0,0,0],
-      style : 'color:blue',
-      modalOpen : false,
-      post : posts,
+      price1 : 60,
+      스타일 : 'color : red',
+      products : ['역삼동원룸', '천호동원룸','마포구원룸'],
+      menus : ['home','products','about'],
+      신고수 : [0,0,0],
+      isModalopen : false,
+      원룸들 : data,
       modalIdx : ''
     }
   },
-  components : {
-    List,
-    Modal
-  },
   methods : {
-    btnClick(i) {
-      this.신고횟수[i]++;
-    }
+    
   }
 }
+
 </script>
 
 
 
 <style>
 * {font-family: 'Noto sans KR', sans-serif; letter-spacing: -0.03rem;font-size: 20px;}
+body {margin : 0;}
+div {box-sizing: border-box;}
 .menu {
-  background : darkslateblue;
+  background : beige;
   padding : 15px;
   border-radius : 5px;
 }
 .menu a {
-  color : white;
+  color : #333;
   padding : 10px;
+  font-weight: bold;
+  border: 1px solid #333;
+  text-transform: uppercase;
 }
-body {
-  margin : 0;
+img {width: 50%;}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
 }
-div {
-  box-sizing: border-box;
-}
-
-img {width: 100%;}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 
 </style>
