79935b71b4e37553f5b12dce2ca1cbb9077d82e0
Seongwon <sngoneee@gmail.com>
2023-11-08 15:07:40 +0900

    study

---
+++ vuestudy/src/components/Home.vue
@@ -0,0 +1,46 @@
+
+
+
+<template>
+    <div>
+      <nav></nav>
+      <div class="container">
+        <h5>React 블로그입니다.</h5>
+        <p>- Vue로 만들었음 -</p>
+      </div>
+  
+    </div>
+  </template>
+  
+  
+  
+  
+  <script>
+  
+  // import List from './components/List.vue'
+  
+  export default {
+    name : 'App',
+    data(){
+      
+    },
+    methods : {
+      
+    },
+    components : {
+      // List
+    }
+  }
+  
+  </script>
+  
+  
+  
+  <style>
+  * {font-family: 'Noto sans KR', sans-serif; letter-spacing: -0.03rem;font-size: 20px;}
+  body {margin : 0;text-align: center;}
+  div {box-sizing: border-box;}
+  .container h5 {font-size: 20px;color: #333;}
+  
+  </style>
+  
