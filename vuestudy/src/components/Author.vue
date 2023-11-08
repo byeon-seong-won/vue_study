c36e357303529bf049617a0716ed5bcc024dd2bb
Seongwon <sngoneee@gmail.com>
2023-11-08 15:07:25 +0900

    study

---
+++ vuestudy/src/components/Author.vue
@@ -0,0 +1,36 @@
+
+
+
+
+<template>
+    <div>
+      <h4>작가소개입니다.</h4>
+      
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name : 'Detail',
+  props : {
+    posts : Array
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods : {
+    
+  },
+  
+}
+
+
+</script>
+
+<style>
+
+</style>
