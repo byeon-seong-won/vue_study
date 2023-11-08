4d88f73f4281d6b968370a7023860bb2bdb5c894
Seongwon <sngoneee@gmail.com>
2023-11-08 11:57:40 +0900

    study

---
+++ vuestudy/src/components/Detail.vue
@@ -0,0 +1,44 @@
+
+
+
+
+<template>
+    <div class="black-bg">
+      <div class="white-bg">
+        <h4>{{해당원룸.title}}</h4>
+        <p>{{해당원룸.content}}만원</p>
+        <p>{{해당원룸.price}}만원</p>
+        <img :src="해당원룸.image" alt="img">
+        <button @click="$emit('modalClose')">닫기</button>
+        <input v-model="month">
+        <button @click="priceCount()">입력하기</button>
+        <p>최종가격 : {{ totalPrice }}</p>
+      </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name : 'Detail',
+  props : {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods : {
+    p
+  },
+  
+}
+
+
+</script>
+
+<style>
+
+</style>
