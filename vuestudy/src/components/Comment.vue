05947944eba2e846071a0f8ae5dac8163e452b8c
Seongwon <sngoneee@gmail.com>
2023-11-08 15:07:30 +0900

    study

---
+++ vuestudy/src/components/Comment.vue
@@ -0,0 +1,36 @@
+
+
+
+
+<template>
+    <div>
+      <h4>댓글입니다.</h4>
+      
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name : 'Detail',
+  props : {
+    posts : Array
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods : {
+    
+  },
+  
+}
+
+
+</script>
+
+<style>
+
+</style>
