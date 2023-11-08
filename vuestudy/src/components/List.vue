845b18d6fe295c47ac1cd9d3d8d7dbe25db958c3
Seongwon <sngoneee@gmail.com>
2023-11-08 15:07:44 +0900

    study

--- vuestudy/src/components/List.vue
+++ vuestudy/src/components/List.vue
@@ -1,21 +1,21 @@
 <template>
     <div>
-      <h5>{{post[0].title}}</h5>
+      <h5 @click="$router.push('/detail/0')">{{posts[0].title}}</h5>
       <p>블로그 글 날짜</p>
     </div>
     <div>
-      <h5>{{post[1].title}}</h5>
+      <h5 @click="$router.push('/detail/1')">{{posts[1].title}}</h5>
       <p>블로그 글 날짜</p>
     </div>
     <div>
-      <h5>{{post[2].title}}</h5>
+      <h5 @click="$router.push('/detail/2')">{{posts[2].title}}</h5>
       <p>블로그 글 날짜</p>
     </div>
   </template>
   <script>
   export default {
     props : {
-        post : Array
+      posts : Array
     }
   }
   </script>
