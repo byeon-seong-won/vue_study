c0a32e2ba1ebbfe77be7491460ff4d24a809e174
Seongwon <sngoneee@gmail.com>
2023-11-10 13:04:06 +0900

    study

---
+++ vuestudy/src/components/Container.vue
@@ -0,0 +1,25 @@
+
+
+
+<template>
+    <div>
+        <Post posts="posts"/>
+        <Post posts="posts"/>
+        <Post posts="posts"/>
+    </div>
+</template>
+   
+<script>
+  import Post from './Post.vue'
+  export default {
+    name : 'App',
+    components : {
+      Post
+    },
+    props : {
+        posts : Array
+    }
+  }
+
+</script>
+  
