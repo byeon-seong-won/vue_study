36bf500aaf9012fac3a1972734dc5171864bfc9e
Seongwon <sngoneee@gmail.com>
2023-11-13 16:36:50 +0900

    study

--- vuestudy/src/store.js
+++ vuestudy/src/store.js
@@ -1,11 +1,28 @@
 import { createStore } from 'vuex'
+import {post} from './assets/Instapost'
+
 
 const store = createStore({
   state(){
     return {
-      name : 'kim'
+      name : 'Byeon',
+      age : 20,
+      like : [1,2,3],
+      likeClick : false,
+      post : post
     }
   },
+  mutations : {
+    likeToggle(state) {
+      if(state.likeClick == false) {
+        state.like[0]++;
+        state.likeClick = true
+      } else if(state.likeClick == true) {
+        state.like[0]--;
+        state.likeClick = false
+      }
+    }
+  }
 })
 
 export default store
